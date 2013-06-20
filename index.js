5d9bed837f03ecc5bb456bc1d62a1ab36cbe612a
Krasimir Tsonev <krasimir@outset.ws>
2013-06-20 09:20:10 +0300

    Shell implementation

--- index.js
+++ index.js
@@ -5,47 +5,55 @@ var io = require('socket.io').listen(port);
 var exec = require('child_process').exec;
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('welcome', {});
+
+	var execCommand = function(command, data) {
+		exec(command, function (error, stdout, stderr) {
+			var result = {
+				stdout: stdout, 
+				stderr: stderr, 
+				command: command,
+				context: process.cwd()
+			};
+			socket.emit("result", result);
+		});
+	}
+	var emitError = function(err, command) {
+		var result = {
+			stdout: '', 
+			stderr: err, 
+			command: command,
+			context: process.cwd()
+		};
+		socket.emit("result", result);
+	}
+	var emitMessage = function(str, command) {
+		var result = {
+			stdout: '', 
+			stderr: str, 
+			command: command,
+			context: process.cwd()
+		};
+		socket.emit("result", result);
+	}
+
+	socket.emit('welcome', {context: process.cwd()});
 	socket.on('command', function (data) {
 		if(data.command) {
 			var command = data.command.toString();
-			exec(command, function (error, stdout, stderr) {
-				var result = {
-					stdout: stdout, 
-					stderr: stderr, 
-					command: command 
-				};
-				socket.emit("result", result);
-			});
+			var commandParts = command.split(" ");
+			var commandName = commandParts.shift();
+			if(commandName === "cd") {
+				try {
+				  	process.chdir(commandParts.join(" "));
+				  	emitMessage('', command);
+				} catch (err) {
+				  	emitError(err.toString(), command);
+				}
+			} else {
+				execCommand(command, data);
+			}
 		} else {
 			socket.emit("result", {error: "Missing command."});
 		}
 	});
 });
-
-// var http = require('http'),
-//     url = require('url'),
-//     exec = require('child_process').exec,
-//     port = 3409;
-
-// http.createServer(function (req, res) {
-// 	req.addListener('end', function () {
-
-// 	});
-// 	var pURL = url.parse(req.url, true);
-// 	var cmd = pURL.query['cmd'];
-
-// 	res.writeHead(200, {'Content-Type': 'application/json'});
-
-// 	if(cmd) {
-// 		var child = exec(cmd, function (error, stdout, stderr) {
-// 			var result = '{"stdout":' + stdout + ',"stderr":"' + stderr + '","cmd":"' + cmd + '"}';
-// 			res.end(result + '\n');
-// 		});
-// 	} else {
-// 		var result = '{"stdout":"' + '' + '","stderr":"' + 'cmd is mandatory' + '","cmd":"' + cmd + '"}';
-// 		res.end(result + '\n');
-// 	}  
-   
-// }).listen(port, '127.0.0.1');
-// console.log('Auxilio is running at http://127.0.0.1:' + port + '/');
