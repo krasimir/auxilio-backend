a7f56a08d8aa33dbda5b2dbfc43bcdb641bd70f2
Krasimir Tsonev <krasimir@outset.ws>
2013-06-22 09:44:42 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
 #!/usr/bin/env node
 
-var port = 3443;
-var io = require('socket.io').listen(port);
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var fs = require("fs");
-var path = require("path");
-var carrier = require('carrier');
-var socket;
-var gitstatus = {};
+var port = 3443,
+	io = require('socket.io').listen(port),
+	spawn = require('child_process').spawn,
+	exec = require('child_process').exec,
+	path = require("path"),
+	readdir = require('./lib/ReadDir'),
+	readdireecursive = require('./lib/ReadDirRecursive'),
+	carrier = require('carrier'),
+	socket,
+	gitstatus = {};
 
-var readdir = function(dir) {
-	try {
-		return fs.readdirSync(dir);
-	} catch(e) {
-		return ['Wrong path.'];
-	}
-}
-var readDirRecursive = function(dir, ignore) {
-	var res = {};
-	try {
-		var items = fs.readdirSync(dir);
-		for(var i=0; item = items[i]; i++) {
-			var ignoreDir = false;
-			if(ignore) {
-				for(var j=0; dirToIgnore = ignore[j]; j++) {
-					if(item === dirToIgnore) ignoreDir = true;
-				}
-			}
-			if(!ignoreDir) {
-				var stat = fs.statSync(dir + "/" + item);
-				if(stat.isDirectory()) {
-					res[item] = readDirRecursive(dir + "/" + item, ignore);
-				} else {
-					res[item] = 'file';
-				}
-			}
-		}
-	} catch(e) {
-		return res;
-	}
-	return res;
-}
 var execCommand = function(command, id, callback) {
 	exec(command, {
 		encoding: 'utf8',
@@ -54,45 +23,13 @@ var execCommand = function(command, id, callback) {
 				stderr: stderr
 			})
 		} else {
-			var result = {
+			socket.emit("result", {
 				stdout: stdout, 
 				stderr: stderr, 
 				command: command,
-				context: process.cwd(),
-				files: readdir(process.cwd()),
-				id: id,
-				git: gitstatus
-			};
-			socket.emit("result", result);
+				id: id
+			});
 		}
-	});
-}
-var emitError = function(err, command, id) {
-	socket.emit("result", {
-		stdout: '', 
-		stderr: err, 
-		command: command,
-		context: process.cwd(),
-		files: readdir(process.cwd()),
-		id: id,
-		git: gitstatus
-	});
-}
-var emitMessage = function(str, command, id) {
-	socket.emit("result", {
-		stdout: '', 
-		stderr: str, 
-		command: command,
-		context: process.cwd(),
-		files: readdir(process.cwd()),
-		id: id,
-		git: gitstatus
-	});
-}
-var updateContext = function() {
-	socket.emit('updatecontext', {
-		context: process.cwd(),
-		git: gitstatus
 	});
 }
 var updateGitStatus = function(callback) {
@@ -132,13 +69,7 @@ io.sockets.on('connection', function (s) {
 
 	socket = s;
 
-	updateGitStatus(function() {
-		socket.emit('welcome', {
-			context: process.cwd(),
-			files: readdir(process.cwd()),
-			git: gitstatus
-		});
-	});
+	socket.emit('welcome');
 	socket.on('command', function(data) {
 		if(data.command) {
 			var command = data.command.toString();
@@ -147,51 +78,47 @@ io.sockets.on('connection', function (s) {
 			if(commandName === "cd") {
 				try {
 				  	process.chdir(commandParts.join(" "));
-				  	updateGitStatus(function() {
-				  		emitMessage('', command, data.id);
-				  	});				  	
+				  	updateContext();		  	
 				} catch (err) {
-				  	emitError(err.toString(), command, data.id);
+				  	socket.emit("result", {
+						stdout: '', 
+						stderr: err.toString(),
+						id: data.id
+					});
 				}
 			} else {
 				execCommand(command, data.id);
 			}
-		} else {
-			emitError("Missing command.");
 		}
 	});
 	socket.on('readdir', function(data) {
 		var dirFiles = readdir(process.cwd() + "/" + data.path);
-		var result = {
-			stdout: '', 
-			stderr: '', 
-			command: '',
-			context: process.cwd(),
-			files: readdir(process.cwd()).concat(dirFiles),
-			git: gitstatus
-		};
-		socket.emit("result", result);
-		socket.emit("showhint", {
-			files: dirFiles,
-			git: gitstatus
-		});
+		socket.emit("readdir", { files: dirFiles });
 	});
 	socket.on("tree", function(data) {
 		var dir = data.dir ? process.cwd() + "/" + data.dir : process.cwd();
-		var dirs = readDirRecursive(dir, [".git", ".svn"]);
-		socket.emit("tree", {result: dirs});
+		var dirs = readdireecursive(dir, [".git", ".svn"]);
+		socket.emit("tree", { result: dirs });
 	});
 
 	// updating git information
 	var forceUpdateContext = function() {
 		setTimeout(function() {
-			updateGitStatus(function() {
-				updateContext();
-				forceUpdateContext();
+			updateContext();
+		}, 2000);
+	}
+	var updateContext = function() {
+		updateGitStatus(function() {
+			socket.emit('updatecontext', {
+				context: process.cwd(),
+				git: gitstatus,
+				files: readdir(process.cwd())
 			});
-		}, 5000);
-	}	
+			forceUpdateContext();
+		});
+	}
 	forceUpdateContext();
+	updateContext();
 
 });
 
